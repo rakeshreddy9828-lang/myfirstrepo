8dd28ee8abd4891e1aabb691fb6929f606364c7d
rakesh <rakeshreddy9828@gmail.com>
2026-09-01 15:24:03 +0000

    java file ready

---
+++ ap.java
@@ -0,0 +1,58 @@
+welcome to java file
+int a= 10;
+int b= 10;
+int c= a+b;
+system.out.println("sum value is "+c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
